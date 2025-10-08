caa3961385288161a7321c80a8beef25e8a893f5
danielkurtsawyer <[19danielsawyer@gmail.com]>
2025-10-08 10:15:27 -0400

    Remove express-validator - not used in this controller since no user text input is required

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,5 +1,4 @@
 const db = require("../db/queries");
-const { body, query, validationResult } = require("express-validator");
 
 const getProductsPage = async (req, res) => {
   const { sort, category } = req.query;
